d04d1c26fd1ca02cd2e216bee62e7ac1e742b7d0
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2020-12-27 17:37:48 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,25 @@
+class Rope {
+    constructor(body1, body2, offsetX, offsetY){
+        
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        
+        var options = {
+            bodyA : body1,
+            bodyB : body2,
+            pointB : {x: this.offsetX, y: this.offsetY}
+        }
+        
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+
+    }
+
+    display(){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y );
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var Ground
+var bob1, bob2, bob3, bob4, bob5
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	Ground = new ground(350,300,250,20);
+
+	bob1 = new bob(300,500,40);
+	bob2 = new bob(300,500,40);
+	bob3 = new bob(300,500,40);
+	bob4 = new bob(300,500,40);
+	bob5 = new bob(300,500,40);
+
+	Chain1 = new Rope(bob1.body, Ground.body, -20*2, 0);
+	Chain2 = new Rope(bob2.body, Ground.body, -20*2, 0);
+	Chain3 = new Rope(bob3.body, Ground.body, -20*2, 0);
+	Chain4 = new Rope(bob4.body, Ground.body, -20*2, 0);
+	Chain5 = new Rope(bob5.body, Ground.body, -20*2, 0);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+	rectMode(CENTER);
+  	background("cyan");
+	
+	Ground.display();
+
+	Chain1.display();
+	Chain2.display();
+	Chain3.display();
+	Chain4.display();
+	Chain5.display();
+
+	bob1.display();
+	bob2.display();
+	bob3.display();
+	bob4.display();
+	bob5.display();
+
+	bob1.x = bob1.body.position.x;
+  	bob1.y = bob1.body.position.y;
+
+
+	Go();
+
+ 	drawSprites();
+ 
+}
+
+function Go(){
+	if (keyDown(UP_ARROW)){
+		Matter.Body.applyForce(bob1.body, bob1.body.position, {x : 2, y : -2});
+	}
+}
+
