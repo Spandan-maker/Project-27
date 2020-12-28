0c2e30e0cab8f984eb7db8256b365a7889d0ecd2
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2020-12-28 12:19:11 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -12,14 +12,19 @@ class Rope {
         
         this.rope = Constraint.create(options);
         World.add(world, this.rope);
-
     }
-
     display(){
         var pointA = this.rope.bodyA.position;
         var pointB = this.rope.bodyB.position;
+        var Anchor1X=pointA.x
+		var Anchor1Y=pointA.y
 
+		var Anchor2X=pointB.x+this.offsetX
+		var Anchor2Y=pointB.y+this.offsetY
         strokeWeight(2);
         line(pointA.x, pointA.y, pointB.x, pointB.y );
+        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+       // strokeWeight(2);
+        //line(pointA.x, pointA.y, pointB.x, pointB.y );
     }
-}
+} 
